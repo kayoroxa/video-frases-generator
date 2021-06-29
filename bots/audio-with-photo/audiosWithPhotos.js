37c8efd3414485d7c56e30a4cf9ae6326883fc4c
caio <kayoroxa@gmai.com>
2021-06-29 17:23:52 -0300

    fix log audio with photos

--- bots/audio-with-photo/audiosWithPhotos.js
+++ bots/audio-with-photo/audiosWithPhotos.js
@@ -3,12 +3,12 @@ const audioWithPhoto = require('./audioWithPhoto')
 async function audiosWithPhotos(frasesRandom) {
   for (const [index, frase] of frasesRandom.entries()) {
     const percentage = ((index + 1) * 100) / frasesRandom.length
-    console.log('[Audio With Photo]', `${percentage}%`, frase.en, index)
     await audioWithPhoto({
       pathFileAudio: `./files/audios/${frase.enFile}.mp3`,
       pathFilePhoto: `./files/images/${frase.enFile}.png`,
       pathExportFile: `./files/videos/(${index}_en) ${frase.enFile}.mp4`,
     })
+    console.log('[Audio With Photo]', `${percentage}%`, frase.en, index)
     await audioWithPhoto({
       pathFileAudio: `./files/audios/${frase.ptFile}.mp3`,
       pathFilePhoto: `./files/images/${frase.enFile}.png`,
