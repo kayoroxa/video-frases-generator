41987430c132af388a74fd34d497a300c23ed815
caio <kayoroxa@gmai.com>
2021-07-02 22:26:03 -0300

    data base node yarn add

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ const audiosWithPhotos = require('./bots/audio-with-photo/audiosWithPhotos')
 const joinVideosCustom = require('./bots/join-videos/joinVideosCustom')
 const getFrasesRandom = require('./utils/getFrasesRandom')
 const clearFolders = require('./utils/clearFolders')
+const startNodeDataBase = require('data-base-node')
+const db = startNodeDataBase()
+
+const clear = false
+const folderClear = ['./files/images', './files/videos']
 
 async function main() {
-  await clearFolders('./files/videos', './files/images')
-  const frasesRandom = await getFrasesRandom({
+  clear && (await clearFolders(...folderClear))
+  const getRandom = await getFrasesRandom({
     pathFolderAudiosRecord: './files/audios',
     txtSentencesPath: './files/frases Regex.txt',
-    quantidade: 5,
+    // quantidade: 100,
+    all: true,
   })
+  console.log(getRandom)
+  const dbFrasesRandom = db.tryLoad('frasesRandom').orStartWith(getRandom)
+  clear && dbFrasesRandom.setValue(getRandom)
+  dbFrasesRandom.save()
+  const frasesRandom = getRandom //|| dbFrasesRandom.value()
+
   console.log(`Got ${frasesRandom.length} random`)
   await htmlPhoto({
     pathFileHtml: './files/index.html',
@@ -26,6 +38,6 @@ async function main() {
       .toLocaleDateString()
       .replace(/\//g, '-')}`,
   })
-  await clearFolders('./files/videos', './files/images')
+  clear && (await clearFolders(...folderClear))
 }
 main()
