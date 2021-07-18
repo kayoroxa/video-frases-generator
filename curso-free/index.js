8a477cca70cd0d81666f19ad880e4fcd61bc7efa
caio <kayoroxa@gmai.com>
2021-07-18 13:57:30 -0300

    numAula automatically

--- curso-free/index.js
+++ curso-free/index.js
@@ -6,15 +6,28 @@ const requireJSON5 = require('require-json5')
 const pathJoin = require('path').join
 const clearFolders = require('../utils/clearFolders')
 
-const numAula = 12
-const clean = false
+const lenPalavrasAprendidas = Object.values(
+  require('./generator-sentences/palavras-aprendidas.json')
+).length
+
+console.log(lenPalavrasAprendidas)
+const numAula = lenPalavrasAprendidas
+const clean = true
 
 async function main() {
   console.log(pathJoin(__dirname, './files/slides'))
   clean &&
     (await clearFolders({
       folders: pathJoin(__dirname, './files/slides'),
-      notFiles: ['1.png', '3.png', '4.png', '5.png', '7.png', '8.png'],
+      notFiles: [
+        '1.png',
+        '1b.png',
+        '3.png',
+        '4.png',
+        '5.png',
+        '7.png',
+        '8.png',
+      ],
     }))
   const pathContent = pathJoin(__dirname, `./frasesAula${numAula}.json`)
   const pathOutputImgs = pathJoin(__dirname, './files/slides')
