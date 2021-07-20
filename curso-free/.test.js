221307f3c963d0fbf11d3212a50be979c0340209
caio <kayoroxa@gmai.com>
2021-07-20 15:56:16 -0300

    better test blocks

--- curso-free/.test.js
+++ curso-free/.test.js
@@ -1,4 +1,22 @@
 const pathJoin = require('path').join
+const _ = require('lodash')
+
+const testTranslation = require(pathJoin(
+  __dirname,
+  './assets/testTranslations.json'
+))
+
+function printArray(array) {
+  const tem = element =>
+    _.intersectionBy(testTranslation, [element], item =>
+      JSON.stringify(item.map(v => v?.toLowerCase()))
+    ).length > 0
+
+  array = _.sortBy(array, v => tem(v))
+  array.forEach(element => {
+    console.log(element, tem(element) ? '✅' : '❌')
+  })
+}
 
 function testar({ numAula }) {
   const frasesAula = require(pathJoin(__dirname, `./frasesAula${numAula}.json`))
@@ -8,8 +26,20 @@ function testar({ numAula }) {
     if (!is) console.log(v.pt, '\n', v.en)
     return is
   })
-  console.log(allSentencesRight)
+  if (allSentencesRight) {
+    const allWordWithTranslation = _.uniqBy(
+      allSentences.reduce((p, v) => {
+        return [
+          ...p,
+          ...v.en.split(' ').map((en, i) => [en, v.pt.split(' ')[i]]),
+        ]
+      }, []),
+      item => JSON.stringify(item)
+    )
+    printArray(allWordWithTranslation)
+  }
+  console.log(allSentencesRight ? 'Size Cool ✅' : 'Size not Cool ❌')
   return allSentencesRight
 }
 
-testar({ numAula: 18 })
+testar({ numAula: 11 })
